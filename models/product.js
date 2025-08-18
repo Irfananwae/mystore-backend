29f160b09f410c5bbf53a588cdb725436c9e7b16
Irfananwae <mi9277576@gmail.com>
2025-08-18 18:26:07 +0530

    Create product.js

---
+++ models/product.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    barcode: {
+        type: String,
+        required: [true, 'Barcode is required.'],
+        unique: true
+    },
+    name: {
+        type: String,
+        required: [true, 'Product name is required.']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Product price is required.'],
+        min: [0, 'Price cannot be negative.']
+    },
+    description: {
+        type: String,
+        required: false
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
