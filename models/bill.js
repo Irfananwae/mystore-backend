1f61692e8a983ba2fa036a472bf0dd81e18b8271
Irfananwae <mi9277576@gmail.com>
2025-08-19 22:05:03 +0530

    Update bill.js

--- models/bill.js
+++ models/bill.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const billSchema = new mongoose.Schema({
     items: [{
+        // Storing denormalized data to keep a record of the sale at that time
+        _id: false, // Don't save the _id for subdocuments
         name: String,
         price: Number,
         quantity: Number
