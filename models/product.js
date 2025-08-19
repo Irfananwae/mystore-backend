264d8ff197902bdda4a41277e91e4228aa128652
Irfananwae <mi9277576@gmail.com>
2025-08-19 22:03:39 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,24 +1,9 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    barcode: {
-        type: String,
-        required: [true, 'Barcode is required.'],
-        unique: true
-    },
-    name: {
-        type: String,
-        required: [true, 'Product name is required.']
-    },
-    price: {
-        type: Number,
-        required: [true, 'Product price is required.'],
-        min: [0, 'Price cannot be negative.']
-    },
-    description: {
-        type: String,
-        required: false
-    }, 
+    barcode: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
     quantity: { type: Number, required: true, default: 0 }
 });
 
