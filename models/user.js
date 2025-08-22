85be361f0c0c41c5d62808d93ae226b7ab80a7e9
Irfananwae <mi9277576@gmail.com>
2025-08-22 10:14:10 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true, // Ensures no duplicate emails
-        index: true
+        index: true, 
         lowercase: true
         
     },
