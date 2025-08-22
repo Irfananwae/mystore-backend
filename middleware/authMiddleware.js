27843877711d9070d25011faeb456ebf8a9e99cf
Irfananwae <mi9277576@gmail.com>
2025-08-22 11:04:08 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,21 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        jwt.verify(bearerToken, process.env.JWT_SECRET, (err, authData) => {
+            if (err) {
+                return res.sendStatus(403); // Forbidden
+            }
+            req.authData = authData;
+            next();
+        });
+    } else {
+        res.sendStatus(403); // Forbidden
+    }
+}
+
+module.exports = { verifyToken };
